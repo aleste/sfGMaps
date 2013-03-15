6e433b0c1e7eebeb268ea26d6651ddf21002c507
Alejandro Steinmetz <asteinmetz78@gmail.com>
2013-03-15 12:00:53 -0300

    First commit

---
+++ src/Aleste/MapsBundle/Controller/DefaultController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Aleste\MapsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        
+        /** @var Ivory\GoogleMapBundle\Model\Map */
+		$map = $this->get('ivory_google_map.map');		
+		
+		//La Plata
+		$map->setCenter(-34.919,-57.954, true);
+		$map->setMapOption('zoom', 13);
+		$map->setLanguage('es');
+		$map->setStylesheetOptions(array(
+    		'width' => '600px',
+    		'height' => '600px'
+		));
+
+		$marker = $this->get('ivory_google_map.marker');		
+		$marker->setPrefixJavascriptVariable('marker_');
+		$marker->setPosition(-34.919,-57.954, true);
+	
+		$marker->setOption('clickable', false);
+		$marker->setOption('flat', true);
+		$marker->setOptions(array(
+		    'clickable' => false,
+		    'flat' => true
+		));
+
+
+		$marker1 = $this->get('ivory_google_map.marker');
+		// Configure your marker options
+		$marker1->setPrefixJavascriptVariable('marker_');
+		$marker1->setPosition(-34.940,-57.954, true);
+		//$marker->setAnimation(Animation::DROP);
+
+		$marker1->setOption('clickable', false);
+		$marker1->setOption('flat', true);
+		$marker1->setOptions(array(
+		    'clickable' => false,
+		    'flat' => true
+		));
+
+		$map->addMarker($marker);
+		$map->addMarker($marker1);
+
+		
+        return array('map'=>$map);
+    }
+}
---
+++ src/Aleste/MapsBundle/MapsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aleste\MapsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MapsBundle extends Bundle
+{
+}
